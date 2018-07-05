b8b383f1cd62831fce76ff309832543646c2fe6d
Amar Enkhbat <38126496+amar-enkhbat@users.noreply.github.com>
2018-07-05 21:30:05 +0900

    asdf

--- basic-neural-network.py
+++ basic-neural-network.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
+import os, struct
 
 ## Random seed
 rgen = np.random.RandomState(1)
@@ -20,6 +21,30 @@ iris = datasets.load_iris()
 X = iris.data[:, [0, 3]]
 y = iris.target
 
+def load_mnist(path, kind='train'):
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path, 
+                               '%s-labels.idx1-ubyte' % kind)
+    images_path = os.path.join(path, 
+                               '%s-images.idx3-ubyte' % kind)
+        
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', 
+                                 lbpath.read(8))
+        labels = np.fromfile(lbpath, 
+                             dtype=np.uint8)
+
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack(">IIII", 
+                                               imgpath.read(16))
+        images = np.fromfile(imgpath, 
+                             dtype=np.uint8).reshape(len(labels), 784)
+        images = ((images / 255.) - .5) * 2
+ 
+    return images, labels
+
+X_train, y_train = load_mnist('./mnist/', kind = 'train')
+X, y = load_mnist('./mnist/', kind = 't10k')
 ## Data standardization
 stdsc = StandardScaler()
 X = stdsc.fit_transform(X)
@@ -29,9 +54,9 @@ X = stdsc.fit_transform(X)
 #       Input layer: 2 nodes
 #       Hidden layer: 5 nodes
 #       Output layer: 3 nodes
-input_nodes = 2
-hidden_nodes = 5
-output_nodes = 3
+input_nodes = 784
+hidden_nodes = 25
+output_nodes = 10
 
 # Number of classes
 K = np.unique(y).astype(int)
@@ -79,7 +104,7 @@ y_coded = one_hot_encoder(y)
 eta = 0.1
 
 # Number of epochs
-epoch = 5000
+epoch = 500
 
 # Cost array
 cost_array = []
@@ -135,29 +160,29 @@ plt.plot(range(len(cost_array)), cost_array)
 plt.show()
 
 # Data plot function
-from matplotlib.colors import ListedColormap
-def plot_decision_regions(X, y, weight_1, weight_2, resolution = 0.01):
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
+# from matplotlib.colors import ListedColormap
+# def plot_decision_regions(X, y, weight_1, weight_2, resolution = 0.01):
+#     markers = ('s', 'x', 'o', '^', 'v')
+#     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+#     cmap = ListedColormap(colors[:len(np.unique(y))])
     
-    # Draw the plane
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+#     # Draw the plane
+#     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+#     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+#     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
     
-    # Predict using the meshgrid
-    a_3, a_2, a_1, Z = predict(np.array([xx1.ravel(), xx2.ravel()]).T, weight_1, weight_2)
-    Z = Z.reshape(xx1.shape)
+#     # Predict using the meshgrid
+#     a_3, a_2, a_1, Z = predict(np.array([xx1.ravel(), xx2.ravel()]).T, weight_1, weight_2)
+#     Z = Z.reshape(xx1.shape)
     
-    # Draw the binary decision range
-    plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
+#     # Draw the binary decision range
+#     plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
+#     plt.xlim(xx1.min(), xx1.max())
+#     plt.ylim(xx2.min(), xx2.max())
 
-    # Plot the data
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha = 0.8, c = colors[idx], marker = markers[idx], label = cl, edgecolor = 'black')
+#     # Plot the data
+#     for idx, cl in enumerate(np.unique(y)):
+#         plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha = 0.8, c = colors[idx], marker = markers[idx], label = cl, edgecolor = 'black')
 
 # Calculate accuracy and confusion matrix
 from sklearn.metrics import confusion_matrix
@@ -168,5 +193,5 @@ print("Accuracy =", str(accuracy))
 print(confusion_matrix(y, Z))
 
 # Data plot
-plot_decision_regions(X, y, weight_1, weight_2)
-plt.show()
+# plot_decision_regions(X, y, weight_1, weight_2)
+# plt.show()
